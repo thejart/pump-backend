8eb57ed90ddd9ee578075a4bf877290574e384f7
Jarrett Thacker <thejart@gmail.com>
2022-05-25 22:07:13 -0400

    more whitespace fixes

--- shitty.php
+++ shitty.php
@@ -9,30 +9,27 @@ class ShitPumper extends Shit {
 
     public function __construct() {
         parent::__construct();
-
         $this->parseRequestParams();
     }
 
-  protected function parseRequestParams() {
-    $this->xValue = (float)$this->getRequestParam('x');
-    $this->yValue = (float)$this->getRequestParam('y');
-    $this->zValue = (float)$this->getRequestParam('z');
+    protected function parseRequestParams() {
+        $this->xValue = (float)$this->getRequestParam('x');
+        $this->yValue = (float)$this->getRequestParam('y');
+        $this->zValue = (float)$this->getRequestParam('z');
 
-    if ($this->getRequestParam('shitstorm')) {
-        $this->type = self::PUMPING;
-    //} elseif ((bool)$this->getRequestParam('healthcheck', false)) {
-    } else {
-        if ($this->xValue > -0.009 && $this->xValue < 0.001 && $this->yValue > -0.009 && $this->yValue < 0.001 && $this->zValue > -0.009 && $this->zValue < 0.001) {
-            $this->type = self::STARTUP;
+        if ($this->getRequestParam('shitstorm')) {
+            $this->type = self::PUMPING;
         } else {
-            $this->type = self::HEALTHCHECK;
+            if ($this->xValue > -0.009 && $this->xValue < 0.001 && $this->yValue > -0.009 && $this->yValue < 0.001 && $this->zValue > -0.009 && $this->zValue < 0.001) {
+                $this->type = self::STARTUP;
+            } else {
+                $this->type = self::HEALTHCHECK;
+            }
         }
     }
-  }
 }
 
 $shitpumper = new ShitPumper();
-
 if ($shitpumper->insertPumpEvent()) {
     echo ":thumbsup:";
 } else {
