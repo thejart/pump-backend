189ac9d5f4778f159f7b9283b7ed0b31edb0abff
Jarrett Thacker <thejart@gmail.com>
2022-05-30 09:57:57 -0400

    rename event constants

--- shitty.php
+++ shitty.php
@@ -18,13 +18,13 @@ class ShitPumper extends Shit {
         $this->zValue = (float)$this->getRequestParam('z');
 
         if ($this->getRequestParam('shitstorm')) {
-            $this->type = self::PUMPING;
+            $this->type = self::EVENT_TYPE_PUMPING;
         } else {
             // TODO: Send across a distinct startup signal (rather than all zeroes)
             if ($this->xValue > -0.009 && $this->xValue < 0.001 && $this->yValue > -0.009 && $this->yValue < 0.001 && $this->zValue > -0.009 && $this->zValue < 0.001) {
-                $this->type = self::STARTUP;
+                $this->type = self::EVENT_TYPE_STARTUP;
             } else {
-                $this->type = self::HEALTHCHECK;
+                $this->type = self::EVENT_TYPE_HEALTHCHECK;
             }
         }
     }
