31fed734ea4b12b91abf71eb63a5e4ea986be04f
Jarrett Thacker <thejart@gmail.com>
2022-06-04 19:04:19 -0400

    add rudimentary test classes

---
+++ tests/ShitShowTest.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/../lib/ShitShow.class.php';
+use PHPUnit\Framework\TestCase;
+
+final class ShitShowTest extends TestCase
+{
+    private $envFile = '.env.testing';
+
+    public function test_constructor_hasNoTwilioSecrets() {
+        $shitShow = new ShitShow($this->envFile);
+
+        $this->assertNull($shitShow->getTwilioNumber(), "twilio number should be null");
+        $this->assertNull($shitShow->getTextNumber(), "text number should be null");
+        $this->assertNull($shitShow->getAuthToken(), "auth token should be null");
+        $this->assertNull($shitShow->getAccountSid(), "account sid should be null");
+    }
+}
