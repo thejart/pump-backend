c72d2d250bb1cbd331cd4c78f2a9d3b9faba0991
Jarrett Thacker <thejart@gmail.com>
2022-06-07 20:41:34 -0400

    update tests

--- tests/ShitPumperTest.php
+++ tests/ShitPumperTest.php
@@ -9,7 +9,7 @@ final class ShitPumperTest extends TestCase
     public function test_constructor_hasNoTwilioSecrets() {
         $shitPumper = new ShitPumper($this->envFile);
 
-        $this->assertNull($shitPumper->getTwilioNumber(), "twilio number should be null");
+        $this->assertNull($shitPumper->getTwilioNumbers(), "twilio number should be null");
         $this->assertNull($shitPumper->getTextNumber(), "text number should be null");
         $this->assertNull($shitPumper->getAuthToken(), "auth token should be null");
         $this->assertNull($shitPumper->getAccountSid(), "account sid should be null");
