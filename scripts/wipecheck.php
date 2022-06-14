9c0a9005c293cc33ee9e2f0d16323a340c4658f5
Jarrett Thacker <thejart@gmail.com>
2022-06-14 13:59:28 -0400

    update path of required files

--- scripts/wipecheck.php
+++ scripts/wipecheck.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/lib/WipeCheck.class.php';
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../lib/WipeCheck.class.php';
 
 use Twilio\Rest\Client;
 
