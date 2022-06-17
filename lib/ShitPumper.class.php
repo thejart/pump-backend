06b266e3ddf41a5252bd2de8018e89cb02dea87a
Jarrett Thacker <thejart@gmail.com>
2022-06-17 13:06:41 -0400

    support test calls to http endpoint

--- lib/ShitPumper.class.php
+++ lib/ShitPumper.class.php
@@ -2,17 +2,24 @@
 require __DIR__ . '/BaseShit.class.php';
 
 class ShitPumper extends BaseShit {
-    protected $xValue;
-    protected $yValue;
-    protected $zValue;
-    protected $type;
+    private $xValue;
+    private $yValue;
+    private $zValue;
+    private $type;
+    private $isTestCall = false;
 
     public function __construct($envFile = '.env') {
         parent::__construct($envFile);
         $this->parseRequestParams();
     }
 
-    protected function parseRequestParams() {
+    private function parseRequestParams() {
+        // Bail early if the request doesn't include required params
+        if (is_null($this->getRequestParam('x')) || is_null($this->getRequestParam('y')) || is_null($this->getRequestParam('z'))) {
+            $this->isTestCall = true;
+            return;
+        }
+
         $this->xValue = (float)$this->getRequestParam('x');
         $this->yValue = (float)$this->getRequestParam('y');
         $this->zValue = (float)$this->getRequestParam('z');
@@ -30,6 +37,10 @@ class ShitPumper extends BaseShit {
     }
 
     public function insertCurrentPumpEvent() {
+        if ($this->isTestCall) {
+            return true;
+        }
+
         return $this->insertPumpEvent($this->xValue, $this->yValue, $this->zValue, $this->type, date("Y-m-d H:i:s", strtotime('now')));
     }
 }
